1869ae91a78979d72124e07634945d2813452016
Yuval Assa <assa.yuval107@gmail.com>
2021-07-31 13:01:20 +0300

    fix post func route

--- src/routes/ParkingRouter.ts
+++ src/routes/ParkingRouter.ts
@@ -1,5 +1,6 @@
 import { saveParking, isParkingSpotSafe } from './../services/ModelOperations';
 import {Router} from 'express';
+import { ParkType } from '../models/ParkEvent';
 
 const router = Router();
 
@@ -9,8 +10,10 @@ router.get('/isAlive', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-        await saveParking(req.body.date, req.body.lon, req.body.lon, true);
-        res.status(200).send();
+        const now = new Date();
+        await saveParking(now, req.body.lon, req.body.lon, ParkType.SAFE);
+        const isSafe = await isParkingSpotSafe(now, req.body.lon, req.body.lat);
+        res.status(200).send(isSafe);
     } catch (e) {
         next(e);
     }
@@ -18,7 +21,7 @@ router.post('/', async (req, res, next) => {
 
 router.post('/accident', async (req, res, next) => {
     try {
-        await saveParking(req.body.date, req.body.lon, req.body.lat, false);
+        await saveParking(new Date(), req.body.lon, req.body.lat, ParkType.ACCIDANT);
         res.status(200).send();
     } catch (e) {
         next(e);
@@ -27,7 +30,7 @@ router.post('/accident', async (req, res, next) => {
 
 router.post('/isSafe', async (req, res, next) => {
     try {
-        const isSafe = await isParkingSpotSafe(req.body.date, req.body.lon, req.body.lat);
+        const isSafe = await isParkingSpotSafe(new Date(), req.body.lon, req.body.lat);
         res.status(200).send(isSafe);
     } catch (e) {
         next(e);
